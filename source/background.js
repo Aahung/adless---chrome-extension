79b1c7852d9ec48525f49e413a6e24ff7efb040b
aaHung <landxh@gmail.com>
2014-01-15 15:13:28 +0800

    Revert "initial commit"
    This reverts commit e00700f640fff2f08151226c23a7828421d9b5a2.

--- source/background.js
+++ source/background.js
@@ -1,6 +1,16 @@
 var urls = [
-	"http://brazil.cityu.edu.hk:8754/pebook-jscript/noRight.js"
+	"1x3x.com",
+	"1qwe3r.com",
+	"alitui.weibo.com",
+	"70e.com",
+	"cpro.baidu",
+	"youyou234.com",
+	"qiyou.com"
 ];
+var urls_formatted = new Array();
+for (var i = 0; i < urls.length; i ++) {
+	urls_formatted[i] = "*://*." + urls[i] + "/*";
+}
 function blockRequest(details) {
   return {cancel: true};
 }
@@ -8,6 +18,6 @@ function blockRequest(details) {
 function updateFilters() {
   if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-  chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: urls}, ['blocking']);
+  chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: urls_formatted}, ['blocking']);
 }
  updateFilters();
